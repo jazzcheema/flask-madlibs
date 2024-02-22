2515b761b17dd0f15f7e5225292a4340badd189c
Jazz Cheema <jaskamalc@gmail.com>
2024-02-21 16:25:33 -0800

    worked on madlibs starter info

--- app.py
+++ app.py
@@ -7,3 +7,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get('/')
+def homepage():
+    """homepage for madlibs website."""
+    website_prompts = silly_story.prompts
+
+    return render_template(
+        'questions.html',
+        website_prompts = website_prompts
+        )
+
